ab141bc81ea359ec49343726b6cc791844dc3249
Ananya Nadendla <ananya.nadendla@gmail.com>
2025-01-22 17:52:05 -0500

    homepage fix

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import ResetPassword from "./components/ResetPassword"; // Import the ResetPassword component
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} /> {/* Root route */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/reset-password" element={<ResetPassword />} /> {/* Add route */}
